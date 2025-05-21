316f8e599e520def476fe76c6ce80e3b821b540a
Brahm Van Houzen <brahmvanh@gmail.com>
2025-05-20 23:27:23 -0400

    ran into some major issues with original repo. accidentally installing tailwind instead of tailwindcss while trying to update last year introduced @emnapi deep into my project and slowly deteriorated my ability to install and update packages through npm up until recently trying to make updates to astro package stopped me from being able to even make file and node_module updates in vscode. lots of restarting my pc. started fresh. added all astro integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,140 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
+import icon from 'astro-icon';
+
+import sitemap from '@astrojs/sitemap';
+
+import netlify from '@astrojs/netlify';
+
+import mdx from '@astrojs/mdx';
+
+import sentry from '@sentry/astro';
+
+import partytown from '@astrojs/partytown';
+
+// const strippedWPUrl =
+//   process.env.WP_URL_SRVR?.replace("https://", "") ??
+//   "southshoretechnical.services";
+
+export default defineConfig({
+	env: {
+		schema: {
+			APP_ROOT_URL_CLNT: envField.string({
+				context: 'client',
+				access: 'public',
+			}),
+			APP_ROOT_URL_SRVR: envField.string({
+				context: 'server',
+				access: 'public',
+			}),
+			WP_URL_SRVR: envField.string({ context: 'server', access: 'public' }),
+			WP_URL_SRVR_PROD: envField.string({
+				context: 'server',
+				access: 'public',
+			}),
+			WP_API_PASSWORD: envField.string({ context: 'server', access: 'secret' }),
+			WP_API_USERNAME: envField.string({ context: 'server', access: 'secret' }),
+			WP_URL_CLNT: envField.string({ context: 'client', access: 'public' }),
+			YOOPERS_UNITED_ROOT_URL: envField.string({
+				context: 'server',
+				access: 'public',
+			}),
+			YOOPERS_UNITED_API_ROOT_URL: envField.string({
+				context: 'server',
+				access: 'public',
+			}),
+			YOOPERS_UNITED_API_TOKEN: envField.string({
+				context: 'server',
+				access: 'secret',
+			}),
+			FAUST_SECRET_KEY: envField.string({
+				context: 'server',
+				access: 'secret',
+			}),
+			STRIPE_SECRET_KEY: envField.string({
+				context: 'server',
+				access: 'secret',
+			}),
+			STRIPE_SIGNING_SECRET: envField.string({
+				context: 'server',
+				access: 'secret',
+			}),
+			STRIPE_PUBLISHABLE_KEY: envField.string({
+				context: 'client',
+				access: 'public',
+			}),
+			DONATE_QCD_URL: envField.string({
+				context: 'server',
+				access: 'public',
+			}),
+			DONATE_CHARITABLE_STOCK_URL: envField.string({
+				context: 'server',
+				access: 'public',
+			}),
+			DONATE_CRYPTO_URL: envField.string({
+				context: 'server',
+				access: 'public',
+			}),
+			UPSTASH_REDIS_URL: envField.string({
+				context: 'server',
+				access: 'secret',
+			}),
+			UPSTASH_REDIS_TOKEN: envField.string({
+				context: 'server',
+				access: 'secret',
+			}),
+		},
+	},
+	site: process.env.SITE || 'https://uwmqt.org',
+
+	output: 'server',
+
+	// vite: {
+	//   server: {
+	//     proxy: {
+	//       "/graphql": {
+	//         target: `${process.env.WP_URL_SRVR_PROD ?? "https://api.uwmqt.org"}`,
+	//         changeOrigin: true,
+	//       },
+	//       "^/wp-content/.*": {
+	//         target: "https://api.uwmqt.org",
+	//         changeOrigin: true,
+	//         secure: true,
+	//         rewrite: (path) => path,
+	//       },
+	//     },
+	//   },
+	// },
+	prefetch: true,
+	image: {
+		domains: [`${process.env.WP_URL_SRVR_PROD ?? 'https://api.uwmqt.org'}`],
+	},
+	integrations: [
+		react({
+			babel: {
+				plugins: [['babel-plugin-react-compiler']],
+			},
+		}),
+		tailwind({ applyBaseStyles: false }),
+		icon(),
+		sitemap(),
+		mdx(),
+		sentry({
+			dsn: process.env.SENTRY_DSN,
+			sourceMapsUploadOptions: {
+				project: 'uw-mqt',
+				authToken: process.env.SENTRY_AUTH_TOKEN,
+			},
+		}),
+		partytown({ config: { debug: false, forward: ['dataLayer.push'] } }),
+	],
+
+	adapter: netlify({
+		imageCDN: false,
+	}),
+});
