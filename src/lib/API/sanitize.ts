e11ff36a0507d0291331077490a96c0db07bfe9c
Brahm Van Houzen <brahmvanh@gmail.com>
2025-11-09 21:24:37 -0500

    removed isomorphic-dompurify from app all together

--- src/lib/API/sanitize.ts
+++ src/lib/API/sanitize.ts
@@ -1,21 +1,45 @@
 // src/lib/API/sanitize.ts
-import DOMPurify from 'isomorphic-dompurify';
 
+// Simple regex-based sanitization for SSR environments
+// This avoids jsdom/DOM dependencies that cause issues in serverless functions
 export function sanitizeHTML(dirty: string): string {
-  return DOMPurify.sanitize(dirty, {
-    ALLOWED_TAGS: [
-      'p', 'br', 'strong', 'b', 'em', 'i', 'u', 'span', 'div',
-      'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
-      'ul', 'ol', 'li',
-      'a', 'img'
-    ],
-    ALLOWED_ATTR: [
-      'href', 'title', 'alt', 'src', 'width', 'height',
-      'class', 'id', 'style'
-    ],
-    ALLOWED_URI_REGEXP: /^(?:(?:(?:f|ht)tps?|mailto|tel|callto|cid|xmpp):|[^a-z]|[a-z+.\-]+(?:[^a-z+.\-:]|$))/i,
-    ADD_ATTR: ['target'],
-    FORBID_ATTR: ['onerror', 'onload', 'onclick', 'onmouseover'],
-    FORBID_TAGS: ['script', 'object', 'embed', 'form', 'input', 'textarea', 'select', 'button']
+  if (typeof dirty !== 'string') {
+    return '';
+  }
+
+  // Remove script tags and their content
+  let clean = dirty.replaceAll(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '');
+
+  // Remove dangerous event handlers
+  clean = clean.replaceAll(/\s*on\w+\s*=\s*["'][^"']*["']/gi, '');
+
+  // Remove javascript: URLs
+  clean = clean.replaceAll(/javascript:/gi, '');
+
+  // Remove style attributes (optional - comment out if you need them)
+  // clean = clean.replaceAll(/\s*style\s*=\s*["'][^"']*["']/gi, '');
+
+  // Remove dangerous tags
+  const dangerousTags = ['script', 'object', 'embed', 'form', 'input', 'textarea', 'select', 'button', 'iframe'];
+  for (const tag of dangerousTags) {
+    const regex = new RegExp(`<\\/?${tag}\\b[^>]*>`, 'gi');
+    clean = clean.replaceAll(regex, '');
+  }
+
+  // Allow only specific tags (more restrictive approach)
+  const allowedTags = new Set(['p', 'br', 'strong', 'b', 'em', 'i', 'u', 'span', 'div', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ul', 'ol', 'li', 'a', 'img']);
+  const tagPattern = /<\/?([a-zA-Z][a-zA-Z0-9]*)\b[^>]*>/g;
+
+  clean = clean.replaceAll(tagPattern, (match, tagName) => {
+    if (allowedTags.has(tagName.toLowerCase())) {
+      // For allowed tags, still remove dangerous attributes
+      let cleanTag = match;
+      cleanTag = cleanTag.replaceAll(/\s*on\w+\s*=\s*["'][^"']*["']/gi, '');
+      cleanTag = cleanTag.replaceAll(/javascript:/gi, '');
+      return cleanTag;
+    }
+    return ''; // Remove disallowed tags
   });
+
+  return clean;
 }
